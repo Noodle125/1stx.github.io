a42729edb3422ca67a24037bd378697822bdfe59
Noodle125 <153040693+Noodle125@users.noreply.github.com>
2024-04-14 09:37:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyADmNV47sId0qdGDiWvl6awe1_5PSRncjM",
   authDomain: "ndx-b1e1d.firebaseapp.com",
+  databaseURL: "https://ndx-b1e1d-default-rtdb.firebaseio.com",
   projectId: "ndx-b1e1d",
   storageBucket: "ndx-b1e1d.appspot.com",
   messagingSenderId: "512322480998",
